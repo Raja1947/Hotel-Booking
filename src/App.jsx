7e18e33a517274cce2242c48452297d0ce1d737c
simpleplantraining <115993891+Raja1947@users.noreply.github.com>
2025-06-29 18:01:43 +0530

    horo section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css'
+import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar'
+import Home from './pages/Home';
 
 function App() {
   
@@ -9,6 +12,13 @@ function App() {
    <>
    <div>
     {!isOwnerPath && <Navbar/>}
+    <div className='min-h-[70vh]'>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+      
+    </div>
+    
    </div>
    
    </>
