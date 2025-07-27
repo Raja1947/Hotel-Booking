ec8897e2630e49010f0c4a45fc576a0e2f9b5a37
simpleplantraining <115993891+Raja1947@users.noreply.github.com>
2025-07-27 14:23:01 +0530

    exclusive offer done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero/Hero'
+import FeaturedDestination from '../components/FeaturedDestination.jsx/FeaturedDestination'
+import ExclusiveOffer from '../components/ExclusiveOffer/ExclusiveOffer'
 
 const Home = () => {
   return (
     <>
       <Hero/>
+      <FeaturedDestination/>
+      <ExclusiveOffer/>
     </>
   )
 }
